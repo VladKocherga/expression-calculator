aaaa3d744e9f3a6356362a45754f4283060b9cff
VladKocherga <vlad.kocherga228@gmail.com>
2021-07-08 22:33:01 +0300

    feat: new bad solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
-
 function expressionCalculator(expr) {
-    // write your solution here
+  expr = expr.replace(/ /g, "");
+  let arr = Array.from(expr);
+  for (let i = 0; i < arr.length; i++) {
+    if (Number(arr[i]) < 10 && Number(arr[i + 1]) < 10) {
+      arr[i] = arr[i] + arr[i + 1];
+      arr.splice(i + 1, 1);
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (Number(arr[i]) == 10 && Number(arr[i + 1]) == 0) {
+      arr[i] = arr[i] + arr[i + 1];
+      arr.splice(i + 1, 1);
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      Number(arr[i]) < 1000000000 &&
+      Number(arr[i + 2]) < 1000000000000 &&
+      arr[i + 1] === "/"
+    ) {
+      arr[i] = arr[i] / arr[i + 2];
+      arr.splice(i + 1, 2);
+      i--;
+    } else if (
+      Number(arr[i]) < 1000000000000 &&
+      Number(arr[i + 2]) < 100000000000 &&
+      arr[i + 1] === "*"
+    ) {
+      arr[i] = arr[i] * arr[i + 2];
+      arr.splice(i + 1, 2);
+      i--;
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      Number(arr[i]) < 100000000000000 &&
+      Number(arr[i + 2]) < 100000000000 &&
+      arr[i + 1] === "-"
+    ) {
+      arr[i] = Number(arr[i]) - Number(arr[i + 2]);
+      arr.splice(i + 1, 2);
+      i--;
+    } else if (
+      Number(arr[i]) < 1000000000000000000 &&
+      Number(arr[i + 2]) < 100000000000000000 &&
+      arr[i + 1] === "+"
+    ) {
+      arr[i] = Number(arr[i]) + Number(arr[i + 2]);
+      arr.splice(i + 1, 2);
+      i--;
+    }
+  }
+
+  arr.join("");
+  return Number(arr);
 }
 
 module.exports = {
-    expressionCalculator
-}
+  expressionCalculator,
+};
